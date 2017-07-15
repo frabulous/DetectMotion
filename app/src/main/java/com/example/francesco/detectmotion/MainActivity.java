966692ef4df00716dc564a317f1d08cf82c68aa4
Kappanneo <pannacciclaudio@gmail.com>
2017-07-16 00:26:12 +0200

    Focusing on positive y acceleration, switched to linear movement sensor, added distance evaluation.

--- app/src/main/java/com/example/francesco/detectmotion/MainActivity.java
+++ app/src/main/java/com/example/francesco/detectmotion/MainActivity.java
@@ -15,48 +15,39 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener, OnClickListener, OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, OnSeekBarChangeListener {
 
     AppCompatActivity context;
-    TextView tvX, tvY;
-    Button resetBtn_normal, resetBtn_game, resetBtn_fast;
+    TextView tv1, tv2;
     int currentDelay;
-    boolean start=true;
-    float xLast, yLast;
+    boolean start=false;
     SensorManager sensorManager;
     Sensor sensor;
-    float sogliaX, sogliaY;
-    TextView tvSogliaX, tvSogliaY, timeY, tvTimeSystem;
+    float sogliaY;
+    long delay = 23;
+    float yPrevius=0;
+    float distance=0;
+    TextView tvSogliaY, timeY, tvTimeSystem, tvdelay;
     long sysTime;
-    SeekBar barraSogliaX, barraSogliaY;
-    TextView x_acceleration, y_acceleration, z_acceleration;
-    int col_base = 0xff000000; //black
-    int col_selected = 0xffddffdd; //green_
+    long startTime= 0;
+    SeekBar barraSogliaY, barraDelay;
+    TextView y_acceleration;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvX=(TextView)findViewById(R.id.txtviewX);
-        tvY=(TextView)findViewById(R.id.txtviewY);
-        resetBtn_normal = (Button)findViewById(R.id.resetBtn);
-        resetBtn_game = (Button)findViewById(R.id.resetBtn_game);
-        resetBtn_fast = (Button)findViewById(R.id.resetBtn_fast);
-        resetBtn_normal.setTextColor(col_selected);
-        currentDelay = SensorManager.SENSOR_DELAY_NORMAL;
-        sogliaX = 1.0f;
-        sogliaY = 1.0f;
-        tvSogliaX = (TextView)findViewById(R.id.txtviewSogliaX);
-        barraSogliaX = (SeekBar)findViewById(R.id.barraSogliaX);
-        tvSogliaX.setText("soglia:" + sogliaX);
-        barraSogliaX.setOnSeekBarChangeListener(this);
+        tv1=(TextView)findViewById(R.id.txtview1);
+        tv2=(TextView)findViewById(R.id.txtview2);
+        currentDelay = SensorManager.SENSOR_DELAY_GAME;
+        sogliaY = 4.0f;
         tvSogliaY = (TextView)findViewById(R.id.txtviewSogliaY);
         barraSogliaY = (SeekBar)findViewById(R.id.barraSogliaY);
-        tvSogliaY.setText("soglia:" + sogliaY);
+        barraDelay = (SeekBar)findViewById(R.id.barraDelay);
+        tvdelay = (TextView)findViewById(R.id.tvdelay);
         barraSogliaY.setOnSeekBarChangeListener(this);
+        barraDelay.setOnSeekBarChangeListener(this);
 
-        x_acceleration = (TextView)findViewById(R.id.Xvalue_label);
         y_acceleration = (TextView)findViewById(R.id.Yvalue_label);
-        z_acceleration = (TextView)findViewById(R.id.Zvalue_label);
         timeY = (TextView)findViewById(R.id.time_label);
         tvTimeSystem = (TextView)findViewById(R.id.timeSystem);
         sysTime = System.nanoTime();
@@ -66,11 +57,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // Get SensorManager instance
         sensorManager = (SensorManager)this.getSystemService(SENSOR_SERVICE);
         // Get ACCELEROMETER sensor
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        // Set button listeners
-        resetBtn_normal.setOnClickListener(this);
-        resetBtn_game.setOnClickListener(this);
-        resetBtn_fast.setOnClickListener(this);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
     }
 
     protected void onResume(){
@@ -91,88 +78,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
 
         //Log.e("Main", "GX="+String.valueOf(event.values[0])+"\nGY="+String.valueOf(event.values[1])+"\nGZ="+String.valueOf(event.values[2]));
-        float xCurrent=event.values[0]; // Get current x
         float yCurrent=event.values[1]; // Get current y
-        if(start){
-            // Initialize last x and y
-            xLast=xCurrent;
-            yLast=yCurrent;
-            start=false;
+        if(yCurrent > sogliaY && !start) {
+            tv1.setText("Acceleration start: "+sysTime+" \ndy = " + yCurrent);
+            startTime= sysTime;
+            yPrevius= yCurrent;
+            start= true;
+            distance=0;
         }
-        else{
-            // Calculate variation between last x and current x, last y and current y
-            float xDelta=xLast-xCurrent;
-            float yDelta=yLast-yCurrent;
-            if(Math.sqrt(xDelta*xDelta/2) > sogliaX)
-                tvX.setText("The device is moved horizontally: \ndx = " +xDelta);
-            if(Math.sqrt(yDelta*yDelta/2) > sogliaY) {
-                tvY.setText("The device is moved vertically: \ndy = " + yDelta);
-                timeY.setText("vertical movement at: 0"+ event.timestamp);
-            }
-
-            // Update last x and y
-            xLast=xCurrent;
-            yLast=yCurrent;
+        else if(yCurrent < yPrevius && start) {
+            tv2.setText("Acceleration end: "+sysTime+" \ndy = " + yCurrent);
+            start= false;
+            float time= (sysTime-startTime)/10;
+            timeY.setText("Distanza: "+ yPrevius*time*time);
         }
 
-        x_acceleration.setText("X: "+ event.values[0]);
-        y_acceleration.setText("Y: "+ event.values[1]);
-        z_acceleration.setText("Z: "+ event.values[2]);
+        y_acceleration.setText("Y: "+ yCurrent);
         sysTime = SystemClock.currentThreadTimeMillis();
         tvTimeSystem.setText("System time: "+ sysTime);
-    }
-
-    @Override
-    public void onClick(View view) {
-        int id = view.getId();
-        onPause();
-        tvX.setText("");
-        tvY.setText("");
-        x_acceleration.setText("X: ");
-        y_acceleration.setText("Y: ");
-        z_acceleration.setText("Z: ");
-        start = true;
-        resetBtn_normal.setTextColor(col_base);
-        resetBtn_game.setTextColor(col_base);
-        resetBtn_fast.setTextColor(col_base);
-
-        if(id == resetBtn_normal.getId()) {
-            resetBtn_normal.setTextColor(col_selected);
-            currentDelay = SensorManager.SENSOR_DELAY_NORMAL;
-        }
-        else if(id == resetBtn_game.getId()) {
-            resetBtn_game.setTextColor(col_selected);
-            currentDelay = SensorManager.SENSOR_DELAY_GAME;
-        }
-        else if(id == resetBtn_fast.getId()) {
-            resetBtn_fast.setTextColor(col_selected);
-            currentDelay = SensorManager.SENSOR_DELAY_FASTEST;
-        }
-
-        onResume();
-
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
         int id = seekBar.getId();
 
-        if(id == barraSogliaX.getId()){
-            //gestisce la seekbar della soglia e fa reset
-            sogliaX = ((float)i)/10 ;
-            tvSogliaX.setText("soglia X: "+ sogliaX);
-            tvX.setText("");
-            tvY.setText("");
-            start = true;
-        }
         if(id == barraSogliaY.getId()){
-            //gestisce la seekbar dellala soglia e fa reset
+            //gestisce la seekbar della soglia e fa reset
             sogliaY = ((float)i)/10 ;
             tvSogliaY.setText("soglia Y: "+ sogliaY);
-            tvX.setText("");
-            tvY.setText("");
-            start = true;
         }
+        else if(id == barraDelay.getId()){
+            delay = (long)i;
+            tvdelay.setText("delay: "+ delay);
+        }
+        tv1.setText("");
+        tv2.setText("");
+        timeY.setText("");
+
     }
 
     @Override
